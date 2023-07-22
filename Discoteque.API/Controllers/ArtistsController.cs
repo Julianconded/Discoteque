0c56968830e334776ce13c7d380e8b89db3c60b9
Julian <juliandelgadoconde@gmail.com>
2023-07-21 23:52:31 -0500

    cloned repo

--- Discoteque.API/Controllers/ArtistsController.cs
+++ Discoteque.API/Controllers/ArtistsController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Discoteque.Business.IServices;
+using Discoteque.Data.Models;
 
 namespace Discoteque.API.Controllers
 {
@@ -11,5 +13,35 @@ namespace Discoteque.API.Controllers
     [ApiController]
     public class ArtistsController : ControllerBase
     {
+        private readonly IArtistsService _artistsService;
+
+        public ArtistsController(IArtistsService artistsService)
+        {
+            _artistsService = artistsService;
+        }
+
+        [HttpGet]
+        [Route("GetAllArtistsAsync")]
+        public async Task<IActionResult> GetAllArtistsAsync()
+        {
+            var artists = await _artistsService.GetArtistsAsync();
+            return Ok(artists);
+        }
+
+
+        [HttpPost]
+        [Route("CreateArtistAsync")]
+        public async Task<IActionResult> CreateArtistAsync(Artist artist)
+        {
+            var result = await _artistsService.CreateArtist(artist);
+            return Ok(result);
+        }
+
+        [HttpPatch]
+        [Route("UpdateArtistAsync")]
+        public async Task<IActionResult> UpdateArtistAsync(Artist artist)
+        {
+            return Ok();
+        }
     }
 }
